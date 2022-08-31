bce2f80313ea1ef6d3d08e6947a00e196a25219e
unknown <contact.rijuan@gmail.com>
2022-08-31 13:15:53 +0600

    dataType

---
+++ dataType.js
@@ -0,0 +1,23 @@
+/* there have two types of data in javaScript
+1. primitive data type
+  primitive : the data type has only value that is primitive data type.
+  a. number
+  b. boolean
+  c. string
+  d. null
+  e. undefined
+2. Non primiive data type
+Non primitive: the type is an special data type of javaScript
+a. Object
+b. Array
+c. Function
+*/
+
+
+// Number
+
+let x = 10;
+let y = 20;
+let z = x + y;
+console.log(z)
+console.log(typeof z);
--- first.js
+++ first.js
@@ -1,3 +1,6 @@
 // Output system of javaScript
+/* if we want to print any value in console then we need to console log it.*/
 
 console.log('Hello world!...')
+// if we want to print something in the browser then we need this command
+document.write("Hello world!...")
